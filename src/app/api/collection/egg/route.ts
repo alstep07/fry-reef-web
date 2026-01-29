71077b864c5f0bee9bf17e64f48f534a19bc5803
alstep07 <sashastepanenko2007@gmail.com>
2026-01-29 10:09:33 +0200

    fix: marketplaces

--- src/app/api/collection/egg/route.ts
+++ src/app/api/collection/egg/route.ts
@@ -4,7 +4,7 @@ export async function GET() {
     name: "FryReef Eggs",
     description:
       "Fish eggs from FryReef. Incubate for 24 hours to hatch into fish with random rarity (Common, Rare, Epic, Legendary, or Mythic)!",
-    image: "https://fryreef.com/images/egg/egg.png",
+    image: "https://fryreef.com/images/apple-touch-icon.png",
     external_link: "https://fryreef.com",
     seller_fee_basis_points: 500, // 5% royalty
     fee_recipient: "0x9Ff507683E602590ad1eD484cb3D62CBb564f99A", // TODO: Replace with your wallet
